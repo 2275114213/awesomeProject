6afe867cccb3a13fdf0fb9308a8e735ff9144784
liuxiaolei@prolliance.cn <liuxiaolei@prolliance.cn>
2021-05-26 20:09:37 +0800

    at work

--- day01/string.go
+++ day01/string.go
@@ -16,9 +16,16 @@ func main() {
 	fmt.Println(msg)
 	// 切片 索引 ascii
 	msg = "abcdefg"
+	//整型   %c	该值对应的unicode码值
+	//布尔型 %t	true或false
+	//通用   %T	打印值的类型
 	fmt.Printf("%T,%#v,%c\n", msg[0], msg[0], msg[0])
 	fmt.Println(msg[1:4])
 	// len 是字节的大小
 	fmt.Println(len(msg))
 	fmt.Println(len(msgRaw))
+
+	// session 过期时间，及顺延方式
+	// 应用与Cas集成
+	// API 梳理
 }
